58885fa4ff240ea00be56075dc5f003295115a5b
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2014-01-19 00:12:48 +0100

    Cosmetics

--- electripyGpio.py
+++ electripyGpio.py
@@ -40,7 +40,6 @@ from lib.electripyUserApi import CHANGE, RISING, FALLING, HARD_INTERRUPTS
 import os
 
 class ElectripyGpio():
-    
     def __init__(self):
         self.uper = uper.UPER(self.mainInterrupt)
             
@@ -72,14 +71,16 @@ class ElectripyGpio():
                 inter.callback(data[1][1])
                 break
     
-    def inputMode(self, pin, mode) :
-        """Sets input mode for digitalRead purpose. Available modes are : INPUT_HIGHZ, INPUT_PULLDOWN, INPUT_PULLUP"""
+    def inputMode(self, pin, mode):
+        """Sets input mode for digitalRead purpose. Available modes are: 
+        INPUT_HIGHZ, INPUT_PULLDOWN, INPUT_PULLUP"""
         self.uper.setPrimary(pins[pin])
         self.uper.pinMode(pins[pin], mode)
         shared.declaredPins[pin] = mode
     
-    def digitalWrite(self,pin, state) :
-        """Sets voltage to +3.3V or Ground on corresponding pin. This function takes two parameters : pin number and it's state that can be HIGH = +3.3V or LOW = Ground"""
+    def digitalWrite(self,pin, state):
+        """Sets voltage to +3.3V or Ground on corresponding pin. This function takes
+        two parameters : pin number and it's state that can be HIGH = +3.3V or LOW = Ground"""
         if shared.declaredPins[pin] != OUTPUT:
             self.uper.setPrimary(pins[pin])
             self.uper.pinMode(pins[pin], OUTPUT)
@@ -87,8 +88,9 @@ class ElectripyGpio():
         
         self.uper.digitalWrite(pins[pin], state)
     
-    def digitalRead(self,pin) :
-        """Reads actual voltage on corresponding pin. There are two possible answers : 0 if pin is connected to the Ground or 1 if positive voltage is detected"""
+    def digitalRead(self,pin):
+        """Reads actual voltage on corresponding pin. There are two possible answers:
+        0 if pin is connected to the Ground or 1 if positive voltage is detected"""
         if (shared.declaredPins[pin] != INPUT_HIGHZ) and (shared.declaredPins[pin] != INPUT_PULLUP) and (shared.declaredPins[pin] != INPUT_PULLDOWN) :
             self.uper.setPrimary(pins[pin])
             self.uper.pinMode(pins[pin], INPUT_HIGHZ)
@@ -96,8 +98,9 @@ class ElectripyGpio():
         
         return self.uper.digitalRead(pins[pin])
     
-    def analogRead(self, pin) :
-        """Reads input on specified Analog to Digital Convertor. ADC is available on pins from 25 to 32 Output is 10bits resolution or from 0-1023"""
+    def analogRead(self, pin):
+        """Reads input on specified Analog to Digital Convertor. ADC is available on pins from
+        25 to 32 Output is 10bits resolution or from 0-1023"""
         if ((pin >= adcs[0]) and (pin <= adcs[-1])):
             if shared.declaredPins[pin] != ADC_INPUT:
                 self.uper.setSecondary(pins[pin])
@@ -105,12 +108,17 @@ class ElectripyGpio():
             
             adcPin = adcs.index(pin)
             return self.uper.analogRead(adcPin)
-        else :
+        else:
             print "*SYSOUT*Error! Pin " + str(pin) + " is not ADC pin, ADCs are on pins 25-32!"
             return -1           
     
     def pwmWrite(self, pin, value) :
-        """Pulse with modulation is available at 6 pins from 19-24 and has 16bits of precision. By default WeIO sets PWM frequency at 20000ms and 8bit precision or from 0-255. This setup is well situated for driving LED lighting. Precision and frequency can be changed separately by calling additional functions for other uses : setPwmPeriod and setPwmLimit. PWM can also drive two different frequencies on two separate banks of 3 pins. For this feature look functions : setPwmPeriod0, setPwmPeriod1, setPwmLimit0 and setPwmLimit1."""
+        """Pulse with modulation is available at 6 pins from 19-24 and has 16bits of precision.
+        By default WeIO sets PWM frequency at 20000ms and 8bit precision or from 0-255.
+        This setup is well situated for driving LED lighting. Precision and frequency can be changed
+        separately by calling additional functions for other uses : setPwmPeriod and setPwmLimit.
+        PWM can also drive two different frequencies on two separate banks of 3 pins.
+        For this feature look functions : setPwmPeriod0, setPwmPeriod1, setPwmLimit0 and setPwmLimit1."""
         if ((pin >= pwms[0]) and (pin <= pwms[2])):
             #port0
             if shared.declaredPins[pin] != PWM0_OUTPUT:
@@ -156,14 +164,13 @@ class ElectripyGpio():
             self.uper.pwm1_set(pwmPin, int(out))
         #print "PWM on ", pwmPin, " value ", out
         
-        else :
+        else:
             print "*SYSOUT*Error! Pin " + str(pin) + " is not PWM pin, PWMs are on pins 19-24!"
     
     def proportion(self, value,istart,istop,ostart,ostop) :
         return float(ostart) + (float(ostop) - float(ostart)) * ((float(value) - float(istart)) / (float(istop) - float(istart)))
     
     def setPwm0PortPeriod(self, period):
-        
         if ((period >= 0) and (period <= 65535)): 
             self.pwm0PortPeriod = period
             if self.pwm0BeginCalled is False:
@@ -182,7 +189,8 @@ class ElectripyGpio():
             print "*SYSOUT*Error! PWM period can be only between 0-65535"
     
     def setPwmPeriod(self, period):
-        """Overrides default value of 20000ms to set new period value for whole 6 PWM pins. Period value must be superior than 0 and inferior than 65535."""
+        """Overrides default value of 20000ms to set new period value for whole 6 PWM pins.
+        Period value must be superior than 0 and inferior than 65535."""
         if ((period >= 0) and (period <= 65535)): 
             self.pwm0PortPeriod = period
             self.pwm1PortPeriod = period
@@ -210,9 +218,9 @@ class ElectripyGpio():
         else:
             print "*SYSOUT*Error! PWM limit can't be superior than " + self.pwm1PortPeriod + " , 0 or inferior than 0"
     
-    
     def setPwmLimit(self, limit):
-        """Overrides default limit of 8bits for PWM precision. This value sets PWM counting upper limit and it's expressed as decimal value. This limit will be applied to all 6 PWM pins.
+        """Overrides default limit of 8bits for PWM precision. This value sets PWM counting
+        upper limit and it's expressed as decimal value. This limit will be applied to all 6 PWM pins.
             """
         if ((limit > 0) and (limit <= self.pwm0PortPeriod) and (limit <= self.pwm1PortPeriod)):
             self.pwm0Limit = limit
@@ -228,15 +236,13 @@ class ElectripyGpio():
             self.uper.attachInterrupt(myid, pins[pin], mode)
     
     def detachInterrupt(self, pin):
-        
         for m in self.interrupts:
             if not(m is None):
                 if (m.pin==pin):
                     #print "pin to be detached ", m.pin
                     self.uper.detachInterrupt(m.myid)
     
-    
-    def getAvailableInterruptId(self) :
+    def getAvailableInterruptId(self):
         for i in range(0,HARD_INTERRUPTS):
             if self.interrupts[i] == None:
                 return i
