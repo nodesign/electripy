c6679fe433b582251f58f2047e13dcde17617df4
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2014-01-19 19:01:14 +0100

    Defines global WeIO-like functions that are wrappers for board custom functions

--- lib/electripyIO.py
+++ lib/electripyIO.py
@@ -1,3 +1,4 @@
+from lib.electripyGlobals import *
 import time
 
 ###
@@ -15,8 +16,8 @@ def mainInterrupt(data):
     # do some pre-init
 
     # Check if the board implements this function
-    if (electripyBoard.mainInterrupt != None):
-        result = electripyBoard.mainInterrupt(data)
+    if (board.mainInterrupt != None):
+        result = board.mainInterrupt(data)
     
     # do some post-init
     
@@ -24,98 +25,98 @@ def mainInterrupt(data):
 
 def inputMode(pin, mode):
     result = None
-    if (electripyBoard.inputMode != None):
-        result = electripyBoard.inputMode(pin, mode)
+    if (board.inputMode != None):
+        result = board.inputMode(pin, mode)
     return result
             
 def digitalWrite(pin, state):
     res = None
-    if (electripyBoard.digitalWrite != None):
-        res = electripyBoard.digitalWrite(pin, state)
+    if (board.digitalWrite != None):
+        res = board.digitalWrite(pin, state)
     return res
     
 def digitalRead(pin):
     res = None
-    if (electripyBoard.digitalRead != None):
-        res = electripyBoard.digitalRead(pin)
+    if (board.digitalRead != None):
+        res = board.digitalRead(pin)
     return res
     
 def analogRead(pin):
     res = None
-    if (electripyBoard.analogRead != None):
-        res = electripyBoard.analogRead(pin)
+    if (board.analogRead != None):
+        res = board.analogRead(pin)
     return res
     
 def pwmWrite(pin, value):
     res = None
-    if (electripyBoard.pwmWrite != None):
-        res = electripyBoard.pwmWrite(pin, value)
+    if (board.pwmWrite != None):
+        res = board.pwmWrite(pin, value)
     return res
 
 def analogWrite(pin, value):
     """Defining synonime of pwmWrite to match arduino syntax"""
     res = None
-    if (electripyBoard.pwmWrite != None):
-        res = electripyBoard.pwmWrite(pin, value)
+    if (board.pwmWrite != None):
+        res = board.pwmWrite(pin, value)
 
 def proportion(value, istart, istop, ostart, ostop):
     res = None
-    if (electripyBoard.proportion != None):
-        res = electripyBoard.proportion(value, istart, istop, ostart, ostop)
+    if (board.proportion != None):
+        res = board.proportion(value, istart, istop, ostart, ostop)
     return res
     
 def setPwm0PortPeriod(period):
     res = None
-    if (electripyBoard.setPwm0PortPeriod != None):
-        res = electripyBoard.setPwm0PortPeriod(period)
+    if (board.setPwm0PortPeriod != None):
+        res = board.setPwm0PortPeriod(period)
     return res
 
 def setPwm1PortPeriod(period):
     res = None
-    if (electripyBoard.setPwm1PortPeriod != None):
-        res = electripyBoard.setPwm1PortPeriod(period)
+    if (board.setPwm1PortPeriod != None):
+        res = board.setPwm1PortPeriod(period)
     return res
 
 def setPwmPeriod(period):
     res = None
-    if (electripyBoard.setPwmPeriod):
-        res = electripyBoard.setPwmPeriod(period)
+    if (board.setPwmPeriod):
+        res = board.setPwmPeriod(period)
     return res
 
 def setPwm0Limit(limit):
     res = None
-    if (electripyBoard.setPwm0Limit != None):
-        res = electripyBoard.setPwm0Limit(limit)
+    if (board.setPwm0Limit != None):
+        res = board.setPwm0Limit(limit)
     return res
 
 def setPwm1Limit(limit):
     res = None
-    if (electripyBoard.setPwm1Limit):
-        res = electripyBoard.setPwm1Limit(limit)
+    if (board.setPwm1Limit):
+        res = board.setPwm1Limit(limit)
     return res
 
 def setPwmLimit(limit):
     res = None
-    if (electripyBoard.setPwmLimit):
-        res = electripyBoard.setPwmLimit(limit)
+    if (board.setPwmLimit):
+        res = board.setPwmLimit(limit)
     return res
 
 def attachInterrupt(pin, mode, callback):
     res = None
-    if (electripyBoard.attachInterrupt != None):
-        res = electripyBoard.attachInterrupt(pin, mode, callback)
+    if (board.attachInterrupt != None):
+        res = board.attachInterrupt(pin, mode, callback)
     return res
 
 def detachInterrupt(pin):
     res = None
-    if (electripyBoard.detachInterrupt != None):
-        res = electripyBoard.detachInterrupt(pin)
+    if (board.detachInterrupt != None):
+        res = board.detachInterrupt(pin)
     return res
 
 def getAvailableInterruptId():
     res = None
-    if (electripyBoard.getAvailableInterruptId):
-        res = electripyBoard.getAvailableInterruptId()
+    if (board.getAvailableInterruptId):
+        res = board.getAvailableInterruptId()
     return res
 
 def delay(period):
